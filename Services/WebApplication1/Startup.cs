0acad80c5a5167e90013e825f9e3ef60c627666f
Amr Yousef <amr.yousef.diab@gmail.com>
2017-03-22 14:20:06 +0200

    Put custom simple inject nuget package instead of using it directly

--- Services/WebApplication1/Startup.cs
+++ Services/WebApplication1/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Framework.Core.CQRS.Core;
+using Framework.SimpleInjector;
 using Framework.CQRS;
 using Rater.Core.Contracts.Queries;
 using Rater.Services.QueryHandler;
